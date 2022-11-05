211b1de3225763de94af5759ba08cadc43465ad3
nickmura <92566574+nickmura@users.noreply.github.com>
2022-11-04 18:19:48 -0600

    Added warning for users with no usernames

--- src/lib/components/_ui/reusable/Navbar.svelte
+++ src/lib/components/_ui/reusable/Navbar.svelte
@@ -20,9 +20,10 @@
 
 	let isOpen = false;
 	let isUser = true;
-
+	let hasName = true
     setTimeout(() => {
 		if (!$connectedAddress) isUser = false
+		if ($connectedAddress && !$connectedUsername) hasName = false
 	}, 1000)
 
 </script>
@@ -30,6 +31,14 @@
 <nav class="relative z-50 flex items-center justify-between py-8">
 	<Logo></Logo>
 	{#if !$connectedAddress && !isUser}
+    <div class="animate-pulse duration-1000 p-2 absolute z-20 max-w-10 left-1/2 -translate-x-1/2 max-w-full text-xs  
+    flex justify-center transition-opacity text-blue-700 bg-blue-100 rounded-lg dark:bg-blue-200 dark:text-blue-800" role="alert">
+        <svg aria-hidden="true" class="flex-shrink-0 inline w-5 h-5 mr-2 " fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7-4a1 1 0 11-2 0 1 1 0 012 0zM9 9a1 1 0 000 2v3a1 1 0 001 1h1a1 1 0 100-2v-3a1 1 0 00-1-1H9z" clip-rule="evenodd"></path></svg>
+        <div class='pt-0.5'>Log into Tronlink via extension first, and switch to Shasta network before connecting.</div>
+    </div>
+    {/if}
+
+	{#if $connectedAddress && !$connectedUsername && !hasName}
     <div class="animate-pulse duration-1000 p-2 absolute z-20 max-w-10 left-1/2 -translate-x-1/2 max-w-full text-xs  
     flex justify-center transition-opacity text-blue-700 bg-blue-100 rounded-lg dark:bg-blue-200 dark:text-blue-800" role="alert">
         <svg aria-hidden="true" class="flex-shrink-0 inline w-5 h-5 mr-2 " fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7-4a1 1 0 11-2 0 1 1 0 012 0zM9 9a1 1 0 000 2v3a1 1 0 001 1h1a1 1 0 100-2v-3a1 1 0 00-1-1H9z" clip-rule="evenodd"></path></svg>
