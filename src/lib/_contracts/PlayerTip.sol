5d0f2a47450503a98f17d642534236afb924435a
nickmura <92566574+nickmura@users.noreply.github.com>
2022-11-23 15:24:42 -0700

    changed contract name

--- src/lib/_contracts/PlayerTip.sol
+++ src/lib/_contracts/PlayerTip.sol
@@ -1,7 +1,7 @@
     //SPDX-License-Identifier: MIT
     pragma solidity ^0.8.7;
 
-    contract TrxMini_Player_Tip {
+    contract Player_Tip {
         event Transfer(uint32 uuid, uint64 amount, address to);
         
         function tip(uint32 uuid, uint64 amount, address payable to) public payable {
