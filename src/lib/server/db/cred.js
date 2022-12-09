fbc15558d3e6192219adce31a2893005ac78854d
nickmura <92566574+nickmura@users.noreply.github.com>
2022-12-09 01:57:16 -0700

    Changed dev endpoints

--- src/lib/server/db/cred.js
+++ src/lib/server/db/cred.js
@@ -3,7 +3,7 @@ import pkg from 'pg'
 const { Client } = pkg;
 // DISCLAIMER - This is handled server side with SHA3-512 passwords, not used.
 export const post = new Client({
-    host: "172.105.106.183",
+    host: "192.53.123.185",
     user: "nick",
     port: 5432,
     password: "admin",
